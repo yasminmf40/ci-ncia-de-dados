5ca1f218d5ee4b9013b79588bf24e1208e67ea45
Yasmin M Freitas <108411121+yasminmf40@users.noreply.github.com>
2024-10-11 08:50:48 -0300

    Update inormaçõeGlobais.js

--- inormaçõeGlobais.js
+++ inormaçõeGlobais.js
@@ -1,6 +1,3 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
-
-async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
