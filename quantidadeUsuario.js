62dd6fa001a95d310fbe044f71eaddeb6d4be9b2
Yasmin M Freitas <108411121+yasminmf40@users.noreply.github.com>
2024-10-11 08:50:21 -0300

    Update quantidadeUsuario.js

--- quantidadeUsuario.js
+++ quantidadeUsuario.js
@@ -1,5 +1,3 @@
-import { getCSS, tickConfig } from "./common.js"
-
 async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
